7647e8dbe14ef8330908eb05a497191c5a72981c
youssef <youssef.chagtab@etu.u-bordeaux.fr>
2015-12-16 00:59:32 +0100

    rectification des entités

--- alda-projet/src/main/java/fr/universite/bordeaux/entities/Announcement.java
+++ alda-projet/src/main/java/fr/universite/bordeaux/entities/Announcement.java
@@ -1,66 +1,75 @@
 package fr.universite.bordeaux.entities;
 
 import java.io.Serializable;
+import javax.persistence.*;
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
+/**
+ * The persistent class for the announcement database table.
+ * 
+ */
 @Entity
-@Table(name="Announcement")
-public class Announcement implements Serializable{
-	private static final long serialVersionUID = 9129298985708507881L;
+@NamedQuery(name="Announcement.findAll", query="SELECT a FROM Announcement a")
+public class Announcement implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private long id;
-	@NotNull
-	private String name;
-	@Size(max=200)
-	private String description;
+	private String id;
+
 	@Temporal(TemporalType.DATE)
 	private Date createdDate;
-	
+
+	private String description;
+
+	private String name;
+
+	//bi-directional many-to-one association to User
 	@ManyToOne
-	@JoinColumn(name="user_id", referencedColumnName="id")
-	User user;
-	
-	public long getId() {
-		return id;
+	private User user;
+
+	public Announcement() {
 	}
-	public void setId(long id) {
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
 		this.id = id;
 	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
+
 	public Date getCreatedDate() {
-		return createdDate;
+		return this.createdDate;
 	}
+
 	public void setCreatedDate(Date createdDate) {
 		this.createdDate = createdDate;
 	}
-	public User getUser() {
-		return user;
+
+	public String getDescription() {
+		return this.description;
 	}
-	public void setUser(User user) {
-		this.user = user;
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
+
 	public String getName() {
-		return name;
+		return this.name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 }
--- alda-projet/src/main/java/fr/universite/bordeaux/entities/User.java
+++ alda-projet/src/main/java/fr/universite/bordeaux/entities/User.java
@@ -1,88 +1,121 @@
 package fr.universite.bordeaux.entities;
 
 import java.io.Serializable;
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
+/**
+ * The persistent class for the user database table.
+ * 
+ */
 @Entity
-@Table(name="User")
-public class User implements Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 8897117888817851683L;
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private long id;
-	@NotNull
-	@Column(name = "email", length = 200)
-	private String email;
-	@Size(max=30)
-	private String firstName;
-	@Column(length=30)
-	private String lastName;
-	@Size(min=8)
-	private String password;
-	//@Past
+	private String id;
+
 	@Temporal(TemporalType.DATE)
 	private Date birthday;
+
 	@Temporal(TemporalType.DATE)
 	private Date dateInscription;
-	
+
+	private String email;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String password;
+
+	//bi-directional many-to-one association to Announcement
 	@OneToMany(mappedBy="user")
-	List<Announcement> announcements;
-	
-	public long getId() {
-		return id;
+	private List<Announcement> announcements;
+
+	public User() {
 	}
-	
-	public String getEmail() {
-		return email;
+
+	public String getId() {
+		return this.id;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+
+	public void setId(String id) {
+		this.id = id;
 	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+
 	public Date getBirthday() {
-		return birthday;
+		return this.birthday;
 	}
+
 	public void setBirthday(Date birthday) {
 		this.birthday = birthday;
 	}
+
 	public Date getDateInscription() {
-		return dateInscription;
+		return this.dateInscription;
 	}
+
 	public void setDateInscription(Date dateInscription) {
 		this.dateInscription = dateInscription;
 	}
-	
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Announcement> getAnnouncements() {
+		return this.announcements;
+	}
+
+	public void setAnnouncements(List<Announcement> announcements) {
+		this.announcements = announcements;
+	}
+
+	public Announcement addAnnouncement(Announcement announcement) {
+		getAnnouncements().add(announcement);
+		announcement.setUser(this);
+
+		return announcement;
+	}
+
+	public Announcement removeAnnouncement(Announcement announcement) {
+		getAnnouncements().remove(announcement);
+		announcement.setUser(null);
+
+		return announcement;
+	}
+
 }
